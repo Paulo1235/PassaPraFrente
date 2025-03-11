b53cabb9b8dff72b6dce8c3c5250ae8391a6767a
talogo-dev <marcogomescardoso@gmail.com>
2025-03-11 20:24:01 +0000

    Creation of login, signin, new password and recover password pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //? CSS
 import './index.css';
 //? Componentes
-import App from './App';
+import Login from './pages/login';
+import SignIn from './pages/signIn'
+import RecoverPass from './pages/recoverPass'
+import NewPassword from './pages/newPassword'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/recoverpass" element={<RecoverPass />} />
+      <Route path="/newpassword" element={<NewPassword />} />
+    </Routes>
+  </BrowserRouter>
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {/* <Login /> */}
+//     <SignIn />
+//     {/* <RecoverPass /> */}
+//     {/* <NewPassword /> */}
+//   </React.StrictMode>
+// );
