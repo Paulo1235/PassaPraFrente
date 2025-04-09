540ce1fc398cd6418a4290084de5971cfb5d8b6d
HugoPereira97 <dhugo045@proton.me>
2025-04-09 17:38:53 +0100

    refactor: validation messages

--- server/src/validations/sale-validation.js
+++ server/src/validations/sale-validation.js
@@ -1,7 +1,18 @@
 import { z } from 'zod'
 
 export const saleSchema = z.object({
-  title: z.string().max(50).min(1).trim(),
-  description: z.string().min(10).max(255),
-  price: z.number().gte(1)
+  title: z
+    .string()
+    .min(1, 'O título é obrigatório e não pode estar vazio.')
+    .max(50, 'O título deve ter no máximo 50 caracteres.')
+    .trim(),
+  description: z
+    .string()
+    .min(10, 'A descrição deve ter no mínimo 10 caracteres.')
+    .max(255, 'A descrição deve ter no máximo 255 caracteres.'),
+
+  price: z
+    .number()
+    .gte(1, 'O preço deve ser maior ou igual a 1.')
+    .max(10000, 'O preço não pode ser maior que 10.000.')
 })
