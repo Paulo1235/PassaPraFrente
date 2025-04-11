d4a7c3f49998887fd8a50b3b70db04d44c21f4f7
HugoPereira97 <138706199+HugoPereira97@users.noreply.github.com>
2025-04-11 18:28:29 +0100

    fix: Update giveaway-repository.js

--- server/src/repositories/giveaway-repository.js
+++ server/src/repositories/giveaway-repository.js
@@ -29,14 +29,17 @@ class GiveawayRepository {
   static async getGiveawayById (id) {
     const pool = await getConnection()
 
-    const giveaway = await pool.request().input('id', sql.Int, id).query(`
-      SELECT Sorteio_ID, Sorteio.Utilizador_ID, DataFim, DataInicio, Titulo, Descricao, NomeCategoria, Condicao, Contacto
-      FROM Sorteio
-      JOIN Utilizador ON Utilizador.Utilizador_ID = Sorteio.Utilizador_ID
-      JOIN Artigo ON Artigo.Artigo_ID = Sorteio.ArtigoArtigo_ID
-      JOIN Categoria ON Categoria.Categoria_ID = Artigo.Categoria_ID
-      JOIN Condicao ON Condicao.Condicao_ID = Artigo.Condicao_ID
-      WHERE Sorteio_ID = @id
+    const giveaway = await pool
+      .request()
+      .input('id', sql.Int, id)
+      .query(`
+        SELECT Sorteio_ID, DataFim, DataInicio, Titulo, Descricao, NomeCategoria, Condicao, Contacto, Sorteio.Utilizador_ID, Estado_ID, Artigo_ID
+        FROM Sorteio
+        JOIN Utilizador ON Utilizador.Utilizador_ID = Sorteio.Utilizador_ID
+        JOIN Artigo ON Artigo.Artigo_ID = Sorteio.ArtigoArtigo_ID
+        JOIN Categoria ON Categoria.Categoria_ID = Artigo.Categoria_ID
+        JOIN Condicao ON Condicao.Condicao_ID = Artigo.Condicao_ID
+        WHERE Sorteio_ID = @id
     `)
 
     return giveaway.recordset[0]
@@ -69,27 +72,38 @@ class GiveawayRepository {
     return availableGiveaways.recordset
   }
 
-  static async updateGiveaway (id, giveaway) {
+  static async updateGiveaway (id, data) {
     const pool = await getConnection()
 
-    const updatedGiveaway = await pool
-      .request()
-      .input('id', sql.Int, id)
-      .input('dataInicio', sql.DateTime, giveaway.startDate)
-      .input('dataFim', sql.DateTime, giveaway.endDate)
-      .input('titulo', sql.VarChar, giveaway.title)
-      .input('descricao', sql.VarChar, giveaway.description)
-      .input('estado', sql.Int, giveaway.state).query(`
-        UPDATE Sorteio
-        SET DataInicio = @dataInicio,
-            DataFim = @dataFim,
-            Titulo = @titulo,
-            Descricao = @descricao,
-            Estado_ID = @estado
-        WHERE Sorteio_ID = @id
-      `)
+    const transaction = pool.transaction()
 
-    return updatedGiveaway.rowsAffected[0] > 0
+    try {
+      await transaction.begin()
+
+      await ItemRepository.updateItem(data.category, data.condition, data.itemId, transaction)
+
+      await transaction
+        .request()
+        .input('id', sql.Int, id)
+        .input('dataInicio', sql.DateTime, data.startDate)
+        .input('dataFim', sql.DateTime, data.endDate)
+        .input('titulo', sql.VarChar, data.title)
+        .input('descricao', sql.VarChar, data.description)
+        .query(`
+          UPDATE Sorteio
+          SET DataInicio = @dataInicio,
+              DataFim = @dataFim,
+              Titulo = @titulo,
+              Descricao = @descricao
+          WHERE Sorteio_ID = @id
+        `)
+
+      await transaction.commit()
+
+      return true
+    } catch (error) {
+      await transaction.rollback()
+    }
   }
 
   static async getUserGiveaways (userId) {
