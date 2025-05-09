4b6a72e6c2f0f5e9370206ddf0864c8f869b13c6
HugoPereira97 <dhugo045@proton.me>
2025-05-09 17:39:24 +0100

    test: approve sale by admin

--- client/src/pages/BPMN/createDraw.jsx
+++ client/src/pages/BPMN/createDraw.jsx
@@ -92,7 +92,7 @@ export default function CreateDraw() {
         <ToastContainer />
         <div className="w-full max-w-[1500px] h-auto min-h-[800px] bg-[#FFFAEE] mx-auto my-10 rounded-xl flex flex-col p-6">
           <div className="button-back flex flex-col items-end">
-              <button className="flex flex-row gap-2 items-center hover:underline" onClick={() => navigate("/index")}>
+              <button className="flex flex-row hover:underline" onClick={() => navigate("/index")}>
                 <Undo2 className="h-5 w-5" />
                 <span>Voltar</span>
               </button>
